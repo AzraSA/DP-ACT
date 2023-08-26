634af9badb286b77d2e2af30b2d9b3ca35140c1e
AzraSA <141994711+AzraSA@users.noreply.github.com>
2023-08-26 12:40:24 +0200

    Update High_risk_case_detection_versus_Passive_Percentage.m

--- Codes for the Face-to-Face Dataset/High_risk_case_detection_versus_Passive_Percentage.m
+++ Codes for the Face-to-Face Dataset/High_risk_case_detection_versus_Passive_Percentage.m
@@ -105,12 +105,12 @@ max_time=max( A2prim(:,1))
 min_time= min( A2prim(:,1))
 BB=(max_time-min_time)/E
 
-
+Bmax=70
 high_risk_percentage_AP_DP3T=zeros(1,Bmax/10);
 high_risk_percentage_DP3T=zeros(1,Bmax/10);
 high_risk_percentage_DPACT=zeros(1,Bmax/10);
 
-Bmax=70
+
 
 for  B=10:10:Bmax
     high_risk_percentage3=0;high_risk_percentage4=0;high_risk_percentage5=0;
